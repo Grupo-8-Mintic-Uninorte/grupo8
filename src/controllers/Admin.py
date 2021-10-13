3e1b0a5312798659456fd181eab9c045794b8cf7
emarrokin <emarroquin@uninorte.edu.co>
2021-10-13 16:58:25 -0500

    Update: pages/admin

--- src/controllers/Admin.py
+++ src/controllers/Admin.py
@@ -2,11 +2,56 @@ from flask import render_template
 
 
 class Admin:
+    def home():
+        return render_template('./pages/admin/admin_home.html')
+
+    #USERS
+    def users():
+        return render_template('./pages/admin/admin_users.html')
+
+    def user(user_id=None):
+        return 'user {0}'.format(user_id)
+
+    def user_new():
+        return 'new user'
+
+    def user_edit(user_id=None):
+        return 'edit user {}'.format(user_id)
+
+    def user_delete(user_id=None):
+        return 'delete user'
+
+    #ACTIVITIES
     def activities():
         return render_template('./pages/admin/admin_activities.html')
 
+    def activity(activity_id=None):
+        return 'activity {}'.format(activity_id)
+
+    def activity_new():
+        return 'new activity'
+
+    def activity_edit(activity_id=None):
+        return 'edit activity {}'.format(activity_id)
+
+    def activity_delete(activity_id=None):
+        return 'delete activity {}'.format(activity_id)
+
+    #COURSES
     def courses():
         return render_template('./pages/admin/admin_courses.html')
+
+    def course(course_id=None):
+        return 'course {}'.format(course_id)
+
+    def course_new():
+        return 'new course'
+
+    def course_edit(course_id=None):
+        return 'edit course {}'.format(course_id)
+
+    def course_delete(course_id=None):
+        return 'delete course {}'.format(course_id)
 
     def profile():
         return render_template('./pages/admin/admin_profile.html')
--- src/controllers/Auth.py
+++ src/controllers/Auth.py
@@ -1,14 +1,18 @@
 from flask import render_template, redirect
 from controllers.Forms import LoginForm, RememberPasswordForm
 
+
 class Auth:
     def login():
         form = LoginForm()
         return render_template('./pages/page_login.html', form=form)
 
+    def logout():
+        return redirect('/')
+
     # @requires_auth
     def remember():
-        form = RememberPasswordForm()()
+        form = RememberPasswordForm()
         return render_template('./pages/page_remember_password.html', form=form)
 
     def submit():
