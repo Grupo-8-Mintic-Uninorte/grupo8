a18cc389df0770a6cd24c320424d0f48afed6e32
emarrokin <emarroquin@uninorte.edu.co>
2021-10-19 10:23:39 -0500

    FIX: typo controller.Database.py

--- src/controllers/Database.py
+++ src/controllers/Database.py
@@ -1,14 +1,20 @@
+# Connect application to sqlite database
+
 class Database:
     def connect():
         return null
+
     def create():
         return null
+
     def read():
         return null
+
     def delete():
         return null
+
     def update():
         return null
+
     def close():
         return null
-                        
