1b9a08822776be3ff9035ce5389e4043c93f4c01
emarrokin <emarroquin@uninorte.edu.co>
2021-10-19 10:12:02 -0500

    FEATURE: add controller.Database.py

---
+++ src/controllers/Database.py
@@ -0,0 +1,14 @@
+class Database:
+    def connect():
+        return null
+    def create():
+        return null
+    def read():
+        return null
+    def delete():
+        return null
+    def update():
+        return null
+    def close():
+        return null
+                        
