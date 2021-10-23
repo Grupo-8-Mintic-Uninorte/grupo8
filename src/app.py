62049ee439a70f9726dd3e433e078d78279067e4
emarrokin <emarroquin@uninorte.edu.co>
2021-10-22 20:36:36 -0500

    UPDATE: Complete notas.db

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 import os
 import jinja_partials
+
 from flask import Flask
+
 project_root = os.path.dirname(__file__)
+
 template_path = os.path.join(project_root, 'templates')
+
 app = Flask(__name__, template_folder=template_path)
+
 app.secret_key = os.urandom(24)
+
 jinja_partials.register_extensions(app)
+
 import routes
 
 if __name__ == '__main__':
