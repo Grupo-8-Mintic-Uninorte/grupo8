b3440d08f83f2d0462915c40881b63d3aae4fda9
emarrokin <emarroquin@uninorte.edu.co>
2021-10-25 11:00:51 -0500

    UPDATE: Some minor changes

--- src/routes.py
+++ src/routes.py
@@ -18,39 +18,41 @@ app.add_url_rule('/password/change', view_func=Auth.change, methods=["GET", "POS
 # Admin Routes
 app.add_url_rule('/admin', view_func=Admin.home)
 
-app.add_url_rule('/professor', view_func=Professor.home_professor)
 
 app.add_url_rule('/admin/users', view_func=Admin.users)
 app.add_url_rule('/admin/users/role/<user_role>', view_func=Admin.users)
-app.add_url_rule('/admin/user/<int:user_id>', view_func=Admin.user)
-app.add_url_rule('/admin/users/new', view_func=Admin.user_new)
+app.add_url_rule('/admin/users/<int:user_id>', view_func=Admin.user)
+app.add_url_rule('/admin/users/new', view_func=Admin.user_new, methods=["GET", "POST"])
 app.add_url_rule('/admin/users/edit/<int:user_id>', view_func=Admin.user_edit)
 app.add_url_rule('/admin/users/delete/<int:user_id>', view_func=Admin.user_delete)
 
 app.add_url_rule('/admin/activities', view_func=Admin.activities)
-app.add_url_rule('/admin/activity/<int:activity_id>', view_func=Admin.activity)
+app.add_url_rule('/admin/activities/<int:activity_id>', view_func=Admin.activity)
 app.add_url_rule('/admin/activities/new', view_func=Admin.activity_new)
 app.add_url_rule('/admin/activities/edit/<int:activity_id>', view_func=Admin.activity_edit)
 app.add_url_rule('/admin/activities/delete/<int:activity_id>', view_func=Admin.activity_delete)
 
 app.add_url_rule('/admin/courses', view_func=Admin.courses)
-app.add_url_rule('/admin/course/<int:course_id>', view_func=Admin.course)
-app.add_url_rule('/admin/courses/new', view_func=Admin.course_new)
+app.add_url_rule('/admin/courses/<int:course_id>', view_func=Admin.course)
+app.add_url_rule('/admin/courses/new', view_func=Admin.course_new, methods=["GET", "POST"])
 app.add_url_rule('/admin/courses/edit/<int:course_id>', view_func=Admin.course_edit)
 app.add_url_rule('/admin/courses/delete/<int:course_id>', view_func=Admin.course_delete)
 
 app.add_url_rule('/admin/profile', view_func=Admin.profile)
 
-app.add_url_rule('/courses/new', view_func=Admin.course_new, methods=["GET", "POST"])
-app.add_url_rule('/users/new', view_func=Admin.user_new, methods=["GET", "POST"])
 
 ##Students routes
 app.add_url_rule('/student', view_func=Student.home_student)
 app.add_url_rule('/student/courses', view_func=Student.courses_student)
 app.add_url_rule('/student/activities', view_func=Student.activities_student)
+app.add_url_rule('/student/profile', view_func=Student.home_student)
+app.add_url_rule('/student/profile/edit', view_func=Student.home_student)
+
 
 ##Professor routes
 app.add_url_rule('/professor', view_func=Professor.home_professor)
+app.add_url_rule('/professor/students', view_func=Professor.students_professor)
 app.add_url_rule('/professor/courses', view_func=Professor.courses_professor)
 app.add_url_rule('/professor/activities', view_func=Professor.activities_professor)
-app.add_url_rule('/professor/students', view_func=Professor.students_professor)
+app.add_url_rule('/professor/profile', view_func=Professor.home_professor)
+app.add_url_rule('/professor/profile/edit', view_func=Professor.home_professor)
